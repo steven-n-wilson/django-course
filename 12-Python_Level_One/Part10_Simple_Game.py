1eaaa36046bc88076a64349c74d8ad7df84fd8e2
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2021-03-10 20:15:05 -0600

    finished game functionality

--- 12-Python_Level_One/Part10_Simple_Game.py
+++ 12-Python_Level_One/Part10_Simple_Game.py
@@ -22,14 +22,76 @@
 # Here are some useful hints:
 
 # Try to figure out what this code is doing and how it might be useful to you
-import random
-digits = list(range(10))
-random.shuffle(digits)
-print(digits[:3])
+from random import randint, shuffle
+# digits = list(range(10))
+# shuffle(digits)
+# print(digits[:3])
 
 # Another hint:
-guess = input("What is your guess? ")
-print(guess)
+# guess = input("What is your guess? ")
+# print(guess)
 
 # Think about how you will compare the input to the random number, what format
 # should they be in? Maybe some sort of sequence? Watch the Lecture video for more hints!
+
+
+def generate_random_num():
+    '''
+    Returns a list of a random three digit number.
+    '''
+    digits = list(range(10))
+    shuffle(digits)
+    return digits[:3]
+
+
+def get_guess():
+    '''
+    Asks to guess a number and returns it in a list.
+    '''
+    raw_guess = input("What is your guess? ")
+    return [int(i) for i in raw_guess]
+
+
+def generate_clues(guess, computer_num):
+    '''
+    Compares user guess to computer num and checks for matches,
+    similarities (Close) or nothing in common (Nope)
+    '''
+
+    if guess == computer_num:
+        print("You have guessed the number!")
+        return "Correct!"
+
+    # Compare guess to computer_num
+    for i in range(3):
+
+        if int(guess[i]) == computer_num[i]:
+            print("Match")
+
+        elif int(guess[i]) in computer_num:
+            print("Close")
+
+        else:
+            print("None")
+
+    return "Continue"
+
+
+# Game Start
+print("Welcome to Code Breaker! Guess a 3 digit number")
+
+# Generate computer_num that will be guessed
+computer_num = generate_random_num()
+print("Code has been generated, take a guess (3 digit number)")
+
+# Empty clue to start the game
+clue = ""
+
+# Keep asking until guess is correct
+while clue != "Correct!":
+
+    # Ask for guess
+    guess = get_guess()
+
+    # Compare guess and give clue
+    clue = generate_clues(guess, computer_num)
