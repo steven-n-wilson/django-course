be8462d4f5afa45b5b283cff26b3949c4bed0267
steven-n-wilson <49218333+steven-n-wilson@users.noreply.github.com>
2021-03-10 20:15:20 -0600

    format change

--- 12-Python_Level_One/Part10_Simple_Game_SOLUTIONS.py
+++ 12-Python_Level_One/Part10_Simple_Game_SOLUTIONS.py
@@ -23,11 +23,13 @@
 
 import random
 
+
 def get_guess():
     '''
     Asks for the number guess and transforms the string to a list.
     '''
     return list(input("What is your guess?"))
+
 
 def generate_code():
     '''
@@ -37,7 +39,8 @@ def generate_code():
     random.shuffle(digits)
     return digits[:3]
 
-def generate_clues(code,userGuess):
+
+def generate_clues(code, userGuess):
     '''
     Takes in a user guess and code then compares the numbers in a loop and
     creates a list of clues according to the matching parameters.
@@ -48,7 +51,7 @@ def generate_clues(code,userGuess):
     clues = []
 
     # Compare guess to code
-    for ind,num in enumerate(userGuess):
+    for ind, num in enumerate(userGuess):
         if num == code[ind]:
             clues.append("Match")
         elif num in code:
@@ -58,13 +61,14 @@ def generate_clues(code,userGuess):
     else:
         return clues
 
+
 # Run Game
 print("Welcome Code Breaker! Let's see if you can guess my 3 digit number!")
 
 # Create a Secret Code to start the Game
 secretCode = generate_code()
 print("Code has been generated, please guess a 3 digit number")
-#print(secretCode)
+# print(secretCode)
 
 # Empty Clue Report to Start with
 clueReport = []
@@ -76,7 +80,7 @@ while clueReport != "CODE CRACKED":
     guess = get_guess()
 
     # Give the clues
-    clueReport = generate_clues(guess,secretCode)
+    clueReport = generate_clues(guess, secretCode)
     print("Here is the result of your guess:")
     for clue in clueReport:
         print(clue)
